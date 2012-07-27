5dd485af210454ca7e038bf38072894b733f4c80
Christopher Aden <christopher.b.aden@gmail.com>
2012-07-26 23:53:32 -0700

    Added bib extension.

--- decrapify.sh
+++ decrapify.sh
@@ -2,4 +2,4 @@
 #LaTeX compiler file cleanup, Christopher Aden Jul 20,2012.
 #Removes all the garbage files produced by LaTeX during compilation.
 #Does not ask to delete, so if you wanted to save any aux|log|synctex|toc, you're SOL.
-find . -name "*.aux" -or -name "*.log" -or -name "*.synctex.gz" -or -name "*.toc" | xargs rm -rf
+find . -name "*.aux" -or -name "*.log" -or -name "*.bib" -or -name "*.synctex.gz" -or -name "*.toc" | xargs rm -rf
