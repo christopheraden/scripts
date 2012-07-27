e0e4234ab6751b9cc45cf800939f7de46c8519e9
Christopher Aden <christopher.b.aden@gmail.com>
2012-07-26 23:48:02 -0700

    Added decrapify.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-scripts
+Christopher Aden's sometimes useful scripts
 =======
+A place I dump files I want people to see. 
---
+++ decrapify.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+#LaTeX compiler file cleanup, Christopher Aden Jul 20,2012.
+#Removes all the garbage files produced by LaTeX during compilation.
+#Does not ask to delete, so if you wanted to save any aux|log|synctex|toc, you're SOL.
+find . -name "*.aux" -or -name "*.log" -or -name "*.synctex.gz" -or -name "*.toc" | xargs rm -rf
